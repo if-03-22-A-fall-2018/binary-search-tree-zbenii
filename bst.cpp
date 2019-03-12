32249ba9222b9e9953bb1ff714e8c1d247c1efdd
zbenii <besic.beni@gmx.at>
2019-03-12 11:21:03 +0100

    first push

--- bst.cpp
+++ bst.cpp
@@ -10,3 +10,107 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "bst.h"
+#include <stdlib.h>
+#include "general.h"
+
+struct Node{
+  int value;
+  struct Node* left;
+  struct Node* right;
+};
+
+Bst new_bst(){
+  Bst newBst=(Bst)malloc(sizeof(struct Node));
+  newBst->left=0;
+  newBst->right=0;
+  newBst->value=0;
+  newBst=0;
+  return newBst;
+}
+
+void delete_bst(Bst bst){
+  if(bst!=0)
+  {
+    delete_bst(bst->left);
+    delete_bst(bst->right);
+    sfree(bst);
+  }
+}
+
+int get_depth(Bst bst){
+  if(bst==0)
+  {
+    return 0;
+  }
+  else
+  {
+    int ldepth=get_depth(bst->left);
+    int rdepth=get_depth(bst->right);
+
+    if(ldepth>rdepth)
+    {
+      return ldepth+1;
+    }
+    else
+    {
+      return rdepth+1;
+    }
+  }
+}
+
+void add(Bst* bst, int value){
+  if((*bst)==0)
+  {
+    Bst currBst=(Bst)malloc(sizeof(struct Node));
+    currBst->left=0;
+    currBst->right=0;
+    currBst->value=value;
+    *bst=currBst;
+  }
+  else if(value<=(*bst)->value)
+  {
+    add(&(*bst)->left,value);
+  }
+  else
+  {
+    add(&(*bst)->right,value);
+  }
+
+}
+
+int root_value(Bst bst){
+  return bst->value;
+}
+
+Bst left_subtree(Bst root){
+  return root->left;
+}
+
+Bst right_subtree(Bst root){
+  return root->right;
+}
+
+int traverse_pre_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+int traverse_in_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+int traverse_post_order(Bst bst, int *elements, int start){
+  return 0;
+}
+
+bool are_equal(Bst bst1, Bst bst2){
+  return true;
+}
+
+void most_left_longest_branch(Bst bst, Bst* branch){
+
+}
+
+int get_number_of_subtrees(Bst bst){
+  return 0;
+}
